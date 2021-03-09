71d51a93cc9295d589a680a0fcd19660474a9f57
kangaroo <qlsdud960604@gmail.com>
2021-03-09 15:07:31 +0900

    이전 페이지 정보 유지 기능 구현

--- src/main/java/com/Board/util/UiUtils.java
+++ src/main/java/com/Board/util/UiUtils.java
@@ -1,10 +1,12 @@
 package com.Board.util;
 
 import com.Board.constant.Method;
+import com.Board.paging.Criteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
@@ -21,5 +23,18 @@ public class UiUtils {
         model.addAttribute("params", params);
 
         return "utils/message-redirect";
+    }
+
+    public Map<String, Object> getPagingParams(Criteria criteria) {
+
+        Map<String, Object> params = new LinkedHashMap<>();
+
+        params.put("currentPageNo", criteria.getCurrentPageNo());
+        params.put("recordsPerPage", criteria.getRecordsPerPage());
+        params.put("pageSize", criteria.getPageSize());
+        params.put("searchType", criteria.getSearchType());
+        params.put("searchKeyword", criteria.getSearchKeyword());
+
+        return params;
     }
 }

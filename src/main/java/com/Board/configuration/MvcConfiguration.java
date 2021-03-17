256668859df51ca6892b631ff1eeabd8f585fa9b
kangaroo <qlsdud960604@gmail.com>
2021-03-17 12:30:13 +0900

    파일 처리용 라이브러리 추가 및 빈(Bean) 설정

--- src/main/java/com/Board/configuration/MvcConfiguration.java
+++ src/main/java/com/Board/configuration/MvcConfiguration.java
@@ -1,7 +1,9 @@
 package com.Board.configuration;
 
 import com.Board.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +13,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
                 .excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
+
+        return multipartResolver;
     }
 }
 

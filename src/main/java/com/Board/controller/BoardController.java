7af771064abaa257cc73fbe8e60571182e400d82
kangaroo <qlsdud960604@gmail.com>
2021-03-20 13:54:17 +0900

    파일 처리에 따른 BoardController 클래스 수정

--- src/main/java/com/Board/controller/BoardController.java
+++ src/main/java/com/Board/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -38,12 +39,12 @@ public class BoardController extends UiUtils {
     }
 
     @PostMapping(value = "/board/register.do")
-    public String registerBoard(@ModelAttribute("params") final BoardDTO params, Model model) {
+    public String registerBoard(@ModelAttribute("params") final BoardDTO params, final MultipartFile[] files, Model model) {
 
         Map<String, Object> pagingParams = getPagingParams(params);
 
         try {
-            boolean isRegistered = boardService.registerBoard(params);
+            boolean isRegistered = boardService.registerBoard(params, files);
 
             if (isRegistered == false) {
                 return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
